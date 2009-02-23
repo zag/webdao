4e9e55de4567b6129f61fd8c68e1f5693e058d04
zag <zag@d2c74b2b-9965-db11-a26f-001731543224>
2009-02-23 17:52:03 +0000

    fix depends
    git-svn-id: file:///home/zag/svn/WebDAO@465 d2c74b2b-9965-db11-a26f-001731543224

--- lib/WebDAO/Store/MLDBM.pm
+++ lib/WebDAO/Store/MLDBM.pm
@@ -4,13 +4,13 @@ package WebDAO::Store::MLDBM;
 
 =head1 NAME
 
-WebDAO::Store::Storable - Implement session store using MLDBM
+WebDAO::Store::MLDBM - Implement session store using MLDBM
 
 =head1 SYNOPSIS
 
 =head1 DESCRIPTION
 
-WebDAO::Store::Storable - Implement session store using MLDBM
+WebDAO::Store::MLDBM - Implement session store using MLDBM
 
 =cut
 
@@ -18,32 +18,10 @@ use File::Path;
 use Fcntl ":flock";
 use IO::File;
 use MLDBM qw (DB_File Data::Dumper);
-use WebDAO::Store::Abstract;
+use WebDAO::Store::Storable
 use Data::Dumper;
 use strict 'vars';
-use base 'WebDAO::Store::Abstract';
-__PACKAGE__->attributes qw/ _dir _cache _is_loaded/;
-
-sub init {
-    my $self = shift;
-    my %pars = @_;
-    die "need param path to dir" unless exists $pars{path};
-    my $dir = $pars{path};
-    $dir .= "/" unless $dir =~ m%/$%;
-    unless ( -d $dir ) {
-    eval {
-        mkpath( $dir, 0 );
-        };
-    if ($@) {   
-        _log1 $self "error mkdir".$@
-    }
-
-    }
-    $self->_dir($dir);
-    my %hash;
-    $self->_cache( \%hash );
-    return 1;
-}
+use base 'WebDAO::Store::Storable';
 
 sub load {
     my $self = shift;
@@ -82,40 +60,5 @@ sub store {
     close DBM;
     return $id;
 
-}
-
-sub _store_attributes {
-    my $self  = shift;
-    my $id    = shift || return;
-    my $ref   = shift || return;
-    my $cache = $self->_cache();
-    while ( my ( $key, $val ) = each %$ref ) {
-        $cache->{$key} = $val;
-    }
-}
-
-sub _load_attributes {
-    my $self = shift;
-    my $id = shift || return;
-    unless ( $self->_is_loaded ) {
-        my $from_storage = $self->load($id);
-        my $cache        = $self->_cache;
-        while ( my ( $key, $val ) = each %$from_storage ) {
-            next if exists $cache->{$key};
-            $cache->{$key} = $val;
-        }
-        $self->_is_loaded(1);
-    }
-    my $loaded = $self->_cache;
-    my %res;
-    foreach my $key (@_) {
-        $res{$key} = $loaded->{$key} if exists $loaded->{$key};
-    }
-    return \%res;
-}
-
-sub flush {
-    my $self = shift;
-    $self->store( @_, $self->_cache );
 }
 1;
